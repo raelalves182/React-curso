8edac5c1566c2feafed14d2e4be9b1ddae21b03b
raelalves182 <ysrael.2009@hotmail.com>
2021-10-18 16:41:40 -0300

    button delete

--- src/api.js
+++ src/api.js
@@ -82,3 +82,29 @@ export function PHOTO_GET(id) {
     },
   };
 }
+
+export function COMMENT_POST(id, body) {
+  return {
+    url: `${API_URL}/api/comment/${id}`,
+    options: {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + window.localStorage.getItem('token '),
+      },
+      body: JSON.stringify(body)
+    },
+  };
+}
+
+export function PHOTO_DELETE(id) {
+  return {
+    url: `${API_URL}/api/photo/${id}`,
+    options: {
+      method: 'DELETE',
+      headers: {
+        Authorization: 'Bearer ' + window.localStorage.getItem('token '),
+      },
+    },
+  };
+}
--- src/components/Photo/PhotoComments.js
+++ src/components/Photo/PhotoComments.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { UserContext } from '../../UserContext';
+import PhotoCommentsForm from './PhotoCommentsForm';
+import styles from './PhotoComments.module.css';
 
-function PhotoComments() {
+function PhotoComments(props) {
+  const [comments, setComments] = React.useState(() => props.comments)
+  const commentSection = React.useRef(null);
+  const { login } = React.useContext(UserContext);
+
+  React.useEffect(() => {
+    commentSection.current.scrollTop = commentSection.current.scrollHeight;
+  }, [comments]);
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <ul ref={commentSection} className={styles.comments}>
+        {comments.map(comment => <li key={comment.comment_ID}>
+          <b>{comment.comment_author}: </b>
+          <span>{comment.comment_content}</span>
+        </li>
+        )}
+      </ul>
+      {login && <PhotoCommentsForm id={props.id} setComments={setComments} />}
+    </>
   )
 }
 
-export default PhotoComments
+export default PhotoComments;
