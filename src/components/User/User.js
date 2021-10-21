8a50beea601d3770a623ee856cefc800fe854128
raelalves182 <ysrael.2009@hotmail.com>
2021-10-21 14:33:18 -0300

    feat(header): add scroll infinite

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -4,13 +4,16 @@ import Feed from '../Feed/Feed'
 import UserPhotoPost from './UserPhotoPost'
 import UserHeader from './UserHeader'
 import UserStats from './UserStats'
+import { UserContext } from '../../UserContext'
 
 const User = () => {
+  const {data} = React.useContext(UserContext);
+
   return (
     <section className="container">
       <UserHeader />
       <Routes>
-        <Route path="/" element={<Feed />} />
+        <Route path="/" element={<Feed user={data.id} />} />
         <Route path="postar" element={<UserPhotoPost />} />
         <Route path="estatisticas" element={<UserStats />} />
       </Routes>
